8c6eda603bff4f6c20f1d7db4151078778966299
Alex <schmidale@hotmail.com>
2015-05-04 10:33:03 +0200

    Discover now BT-Modules an list them.

--- RemoteControlForSumoBot/app/src/main/java/com/asds/remotecontrolforsumobot/MainActivity.java
+++ RemoteControlForSumoBot/app/src/main/java/com/asds/remotecontrolforsumobot/MainActivity.java
@@ -1,27 +1,46 @@
 package com.asds.remotecontrolforsumobot;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private static final int REQUEST_ENABLE_BT = 1;
 
+    /**
+     * Newly discovered devices
+     */
+    private ArrayAdapter<String> mArrayAdapter;
+    BroadcastReceiver mReceiver;
+    BluetoothAdapter mBluetoothAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ArrayAdapter<String> mArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        ListView listView = (ListView) findViewById(R.id.DeviceList);
+
+        listView.setAdapter(mArrayAdapter);
     }
 
     public void enableBluetooth(View v) {
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null)
         {
             // Device does not support Bluetooth
@@ -34,6 +53,25 @@ public class MainActivity extends ActionBarActivity {
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
             }
         }
+    }
+
+    public void discoverDevice(View v) {
+
+        BroadcastReceiver mReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                // When discovery finds a device
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    // Get the BluetoothDevice object from the Intent
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    // Add the name and address to an array adapter to show in a ListView
+                    mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+                }
+            }
+        };
+        // Register the BroadcastReceiver
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
     }
 
 
@@ -57,5 +95,18 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Make sure we're not doing discovery anymore
+        if (mBluetoothAdapter != null) {
+            mBluetoothAdapter.cancelDiscovery();
+        }
+
+        // Unregister broadcast listeners
+        this.unregisterReceiver(mReceiver);
     }
 }
