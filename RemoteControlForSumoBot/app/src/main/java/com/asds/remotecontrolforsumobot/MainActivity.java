8f76c2218aa2479a86f38c2322b168fdd04e1861
Alex <schmidale@hotmail.com>
2015-05-08 09:59:11 +0200

    mReceiver isn't used anymore.

--- RemoteControlForSumoBot/app/src/main/java/com/asds/remotecontrolforsumobot/MainActivity.java
+++ RemoteControlForSumoBot/app/src/main/java/com/asds/remotecontrolforsumobot/MainActivity.java
@@ -131,7 +131,7 @@ public class MainActivity extends Activity {
         }
 
         // Unregister broadcast listeners
-        this.unregisterReceiver(mReceiver);
+        //this.unregisterReceiver(mReceiver);
     }
 
 
